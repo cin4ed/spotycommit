30d5d21c58b8f6fd207655cd28f2febc24cb06c9
Kenneth Quintero <kennethgqv@gmail.com>
2022-12-31 02:03:26 -0700

    Add request function

--- util.php
+++ util.php
@@ -3,3 +3,9 @@
 function say($message) {
 	echo $message . '<br />';
 }
+
+function request($method, $path) {
+	$request_uri = $_SERVER['REQUEST_URI'];
+	$url = parse_url($request_uri);
+	return ($_SERVER['REQUEST_METHOD'] == $method && $url['path'] == $path);
+}
