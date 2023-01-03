8ca1778646922d181e396b09179ccd9072878532
Kenneth Quintero <kennethgqv@gmail.com>
2023-01-03 16:09:23 -0700

    Update: implement oauth.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 require('util.php');
+require('oauth.php');
 
 // GET /
 if (request('GET', '/')) {
@@ -18,33 +19,7 @@ if (request('GET', '/')) {
 
 // GET /auth
 if (request('GET', '/auth')) {
-	$env_vars = parse_ini_file('.env');
-
-	$string = md5(mt_rand());
-	$state = base64_encode($string);
-
-	// store state in session for validation in callback.php
-	session_start();
-	$_SESSION['state'] = $state;
-
-	$query_parameters = [
-		'client_id' => $env_vars['CLIENT_ID'],
-		'response_type' => 'code',
-		'redirect_uri' => $env_vars['REDIRECT_URI'],
-		'state' => $state,
-		'scope' => [
-			'user-read-private user-read-email user-read-recently-played'
-		]
-	];
-
-	$query_parameters_formated = http_build_query($query_parameters);
-
-	say($query_parameters_formated);
-
-	$authorization_url = 'https://accounts.spotify.com/authorize';
-
-	header('Location: ' . $authorization_url . '?' . $query_parameters_formated);
-	exit();
+	redirect_to_auth();
 }
 
 // GET /callback
