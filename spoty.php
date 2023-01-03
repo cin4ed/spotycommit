5335a734e9edb2cc471f3902c316a4adff19d457
Kenneth Quintero <kennethgqv@gmail.com>
2023-01-03 12:25:33 -0700

    is_user check if user exists in db

--- spoty.php
+++ spoty.php
@@ -1,9 +1,31 @@
 <?php
 
 function is_user(): boolean {
-	$user = $_SESSION['spotify_user_id'];
-	return isset($user);
-}
+	$user_id = $_SESSION['spotify_user_id'];
 
+	if (!isset($user_id)) {
+		return false;
+	}
+
+	$env_vars = parse_ini_file('.env');
+
+	$server = $env_vars['SQL_SERVERNAME'];
+	$dbname = $env_vars['SQL_DATABASE'];
+	$conn_str = "mysql:host={$server};dbname={$dbname}";
+
+	$username = $env_vars['SQL_USERNAME'];
+
+	try {
+	    $dbh = new PDO($conn_str, $username);
+	} catch (PDOException $e) {
+	    die('Could not connect: ' . $e->getMessage());
+	}
+
+	$stmt = $dbh->prepare('SELECT * FROM users WHERE user_id = ? LIMIT 1');
+	$stmt->execute([$user_id]);
+	$row = $stmt->fetch();
+
+	return isset($row);
+}
 
 ?>
