5d6f510a29872ef21df9a3d171f17ba098f32b08
Kenneth Quintero <kennethgqv@gmail.com>
2023-01-03 16:06:15 -0700

    Add: redirect_to_auth

--- oauth.php
+++ oauth.php
@@ -1,3 +1,29 @@
 <?php
+function redirect_to_auth() {
+	$env_vars = parse_ini_file('.env');
 
+	$string = md5(mt_rand());
+	$state = base64_encode($string);
+
+	// store state in session for validation in callback.php
+	session_start();
+	$_SESSION['state'] = $state;
+
+	$query_parameters = [
+		'client_id' => $env_vars['CLIENT_ID'],
+		'response_type' => 'code',
+		'redirect_uri' => $env_vars['REDIRECT_URI'],
+		'state' => $state,
+		'scope' => [
+			'user-read-private user-read-email user-read-recently-played'
+		]
+	];
+
+	$query_parameters_formated = http_build_query($query_parameters);
+
+	$authorization_url = 'https://accounts.spotify.com/authorize';
+
+	header('Location: ' . $authorization_url . '?' . $query_parameters_formated);
+	exit();
+}
 ?>
