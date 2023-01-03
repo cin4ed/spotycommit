09e926d40dd01aacedb2750071d76273563486af
Kenneth Quintero <kennethgqv@gmail.com>
2023-01-03 15:58:52 -0700

    Add: save_user

--- spoty.php
+++ spoty.php
@@ -30,4 +30,25 @@ function is_user(): bool {
 
 	return $row != false;
 }
+
+function save_user($user_id, $refresh_token): bool {
+	$env_vars = parse_ini_file('.env');
+
+	$server = $env_vars['SQL_SERVERNAME'];
+	$dbname = $env_vars['SQL_DATABASE'];
+	$conn_str = "mysql:host={$server};dbname={$dbname}";
+
+	$username = $env_vars['SQL_USERNAME'];
+
+	try {
+	    $dbh = new PDO($conn_str, $username);
+	} catch (PDOException $e) {
+	    die('Could not connect: ' . $e->getMessage());
+	}
+
+	$stmt = $dbh->prepare('INSERT INTO users (user_id, refresh_token) VALUES (?, ?)');
+	$result = $stmt->execute([$user_id, $refresh_token]);
+
+	return $result;
+}
 ?>
